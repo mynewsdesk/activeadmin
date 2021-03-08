292088c866e718b37a24b60e425ee02ef0f1e439
David Backeus <duztdruid@gmail.com>
2021-03-08 11:39:24 +0100

    Patch modal-dialog to handke batch_update_attr and set fixed modal width
    Previously done as a monkey patch inside of mnd-audience. However in later versions it became impossible to monkey patch due to ES module encapsulation so changed strategy to forking activeadmin instead.

--- app/javascript/active_admin/lib/modal-dialog.js
+++ app/javascript/active_admin/lib/modal-dialog.js
@@ -1,40 +1,55 @@
+// Patched version of (an old version of) https://git.io/fjf1b to support the custom
+// "batch_update_attr" input type and force the modal width to 600.
+
 function ModalDialog(message, inputs, callback){
   let html = `<form id="dialog_confirm" title="${message}"><ul>`;
   for (let name in inputs) {
-    var opts, wrapper;
+    var elem, opts, wrapper;
     let type = inputs[name];
-    if (/^(datepicker|checkbox|text|number)$/.test(type)) {
+    if (/^(datepicker|checkbox|text|number|batch_update_attr)$/.test(type)) {
       wrapper = 'input';
     } else if (type === 'textarea') {
       wrapper = 'textarea';
     } else if ($.isArray(type)) {
-      [wrapper, opts, type] = ['select', type, ''];
+      [wrapper, elem, opts, type] = ['select', 'option', type, ''];
+    } else if ($.isPlainObject(type) && type.multiple) {
+      [wrapper, elem, opts, type] = ['select', 'option', type.multiple, 'multiple']
     } else {
       throw new Error(`Unsupported input type: {${name}: ${type}}`);
     }
 
-    let klass = type === 'datepicker' ? type : '';
-    html += `<li>
-      <label>${name.charAt(0).toUpperCase() + name.slice(1)}</label>
-      <${wrapper} name="${name}" class="${klass}" type="${type}">` +
-        (opts ? ((() => {
-          const result = [];
+    if (type === 'batch_update_attr') {
+      html += `<li>
+        <label>${name.charAt(0).toUpperCase() + name.slice(1)}</label>
+        <input id="${name}" name="${name}" class="">
+        <label for="${name}_overwrite">overwrite?</label>
+        <input id="${name}_overwrite" name="${name}_overwrite" class="" type="checkbox">
+      </li>`;
+    }
+    else {
+      var klass = type === 'datepicker' ? type : '';
+      html += `<li>
+        <label>${name.charAt(0).toUpperCase() + name.slice(1)}</label>
+        <${wrapper} name="${name}" class="${klass}" type="${type}" ${type === 'multiple' && 'multiple size=10'}>` +
+          (opts ? ((() => {
+            const result = [];
 
-          opts.forEach(v => {
-            const $elem = $('<option></option>');
-            if ($.isArray(v)) {
-              $elem.text(v[0]).val(v[1]);
-            } else {
-              $elem.text(v);
-            }
-            result.push($elem.wrap('<div></div>').parent().html());
-          });
+            opts.forEach(v => {
+              const $elem = $(`<${elem}/>`);
+              if ($.isArray(v)) {
+                $elem.text(v[0]).val(v[1]);
+              } else {
+                $elem.text(v);
+              }
+              result.push($elem.wrap('<div>').parent().html());
+            });
 
-          return result;
-        })()).join('') : '') +
-      `</${wrapper}>` +
-    "</li>";
-    [wrapper, opts, type, klass] = []; // unset any temporary variables
+            return result;
+          })()).join('') : '') +
+        `</${wrapper}>` +
+      "</li>";
+    }
+    [wrapper, elem, opts, type, klass] = []; // unset any temporary variables
   }
 
   html += "</ul></form>";
@@ -44,6 +59,7 @@ function ModalDialog(message, inputs, callback){
 
   form.dialog({
     modal: true,
+    width: 600,
     open(_event, _ui) {
       $('body').trigger('modal_dialog:after_open', [form]);
     },
